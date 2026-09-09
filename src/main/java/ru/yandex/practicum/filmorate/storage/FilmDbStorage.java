8eaa4744747d94268dc371a9965b40e0f9234fca
Владислав Белоус <vladbeloysk@gmail.com>
2026-09-09 22:12:00 +0300

    Checkstyle исправление

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
@@ -38,22 +38,15 @@ public class FilmDbStorage extends BaseDbStorage<Film> implements FilmStorage {
     private static final String GET_TOP_QUERY = "SELECT fl.film_id, name, description, release_date, " + "duration, fl.rating_id, r.name_rating, COUNT(lf.user_id) AS likes_count " + "FROM liked_film AS lf " + "RIGHT JOIN films AS fl ON lf.film_id = fl.film_id " + "LEFT JOIN rating AS r ON fl.rating_id = r.rating_id " + "GROUP BY fl.film_id, fl.name, fl.description, fl.release_date, fl.duration, fl.rating_id, r.name_rating " + "ORDER BY likes_count DESC, fl.film_id ASC LIMIT ?";
     private static final String ADD_GENRE_QUERY = "INSERT INTO film_genres (film_id, genre_id) VALUES (?, ?)";
     private static final String CHECK_LIKE_QUERY = "SELECT COUNT(*) FROM liked_film WHERE film_id = ?";
-    private static final String GET_COMMON_FILMS_QUERY = "SELECT fl.film_id, fl.name, fl.description, fl.release_date, " +
-            "fl.duration, fl.rating_id, r.name_rating, " +
-            "COUNT(lf.user_id) AS likes_count " +
-            "FROM films fl " +
-            "LEFT JOIN rating r ON fl.rating_id = r.rating_id " +
-            "LEFT JOIN liked_film lf ON fl.film_id = lf.film_id " +
-            "WHERE EXISTS ( " +
-            "    SELECT 1 FROM liked_film lf1 " +
-            "    WHERE lf1.film_id = fl.film_id AND lf1.user_id = ? " +
-            ") AND EXISTS ( " +
-            "    SELECT 1 FROM liked_film lf2 " +
-            "    WHERE lf2.film_id = fl.film_id AND lf2.user_id = ? " +
-            ") " +
-            "GROUP BY fl.film_id, fl.name, fl.description, fl.release_date, " +
-            "fl.duration, fl.rating_id, r.name_rating " +
-            "ORDER BY likes_count DESC, fl.film_id ASC";
+    private static final String GET_COMMON_FILMS_QUERY = "SELECT fl.film_id, fl.name, fl.description, fl.release_date, " + "fl.duration, fl.rating_id, r.name_rating, " + "COUNT(lf.user_id) AS likes_count " + "FROM films fl " + "LEFT JOIN rating r ON fl.rating_id = r.rating_id " + "LEFT JOIN liked_film lf ON fl.film_id = lf.film_id " + "WHERE EXISTS ( " + "    SELECT 1 FROM liked_film lf1 " + "    WHERE lf1.film_id = fl.film_id AND lf1.user_id = ? " + ") AND EXISTS ( " + "    SELECT 1 FROM liked_film lf2 " + "    WHERE lf2.film_id = fl.film_id AND lf2.user_id = ? " + ") " + "GROUP BY fl.film_id, fl.name, fl.description, fl.release_date, " + "fl.duration, fl.rating_id, r.name_rating " + "ORDER BY likes_count DESC, fl.film_id ASC";
+    private static final String ADD_DIRECTOR_QUERY = "INSERT INTO film_directors (film_id, director_id) VALUES (?, ?)";
+    private static final String GET_FILM_DIRECTORS_QUERY = "SELECT d.director_id, d.name " + "FROM film_directors fd " + "JOIN directors d ON fd.director_id = d.director_id " + "WHERE fd.film_id = ?";
+    private static final String DELETE_FILM_DIRECTORS_QUERY = "DELETE FROM film_directors WHERE film_id = ?";
+    private static final String GET_FILMS_BY_DIRECTOR_YEAR_QUERY = "SELECT fl.*, rt.name_rating " + "FROM films fl " + "LEFT JOIN rating rt ON fl.rating_id = rt.rating_id " + "JOIN film_directors fd ON fl.film_id = fd.film_id " + "WHERE fd.director_id = ? " + "ORDER BY fl.release_date ASC, fl.film_id ASC";
+    private static final String GET_FILMS_BY_DIRECTOR_LIKES_QUERY = "SELECT fl.*, rt.name_rating, COUNT(lf.user_id) AS likes_count " + "FROM films fl " + "LEFT JOIN rating rt ON fl.rating_id = rt.rating_id " + "JOIN film_directors fd ON fl.film_id = fd.film_id " + "LEFT JOIN liked_film lf ON fl.film_id = lf.film_id " + "WHERE fd.director_id = ? " + "GROUP BY fl.film_id, fl.name, fl.description, fl.release_date, " + "fl.duration, fl.rating_id, rt.name_rating " + "ORDER BY likes_count DESC, fl.film_id ASC";
+    private static final String SEARCH_BY_TITLE_QUERY = "SELECT fl.film_id, fl.name, fl.description, fl.release_date, " + "fl.duration, fl.rating_id, r.name_rating, " + "COUNT(DISTINCT lf.user_id) AS likes_count " + "FROM films AS fl " + "LEFT JOIN rating AS r ON fl.rating_id = r.rating_id " + "LEFT JOIN liked_film AS lf ON fl.film_id = lf.film_id " + "WHERE LOWER(fl.name) LIKE LOWER(?) " + "GROUP BY fl.film_id, fl.name, fl.description, fl.release_date, " + "fl.duration, fl.rating_id, r.name_rating " + "ORDER BY likes_count DESC, fl.film_id ASC";
+    private static final String SEARCH_BY_DIRECTOR_QUERY = "SELECT fl.film_id, fl.name, fl.description, fl.release_date, " + "fl.duration, fl.rating_id, r.name_rating, " + "COUNT(DISTINCT lf.user_id) AS likes_count " + "FROM films AS fl " + "LEFT JOIN rating AS r ON fl.rating_id = r.rating_id " + "LEFT JOIN liked_film AS lf ON fl.film_id = lf.film_id " + "JOIN film_directors AS fd ON fl.film_id = fd.film_id " + "JOIN directors AS d ON fd.director_id = d.director_id " + "WHERE LOWER(d.name) LIKE LOWER(?) " + "GROUP BY fl.film_id, fl.name, fl.description, fl.release_date, " + "fl.duration, fl.rating_id, r.name_rating " + "ORDER BY likes_count DESC, fl.film_id ASC";
+    private static final String SEARCH_BY_TITLE_AND_DIRECTOR_QUERY = "SELECT fl.film_id, fl.name, fl.description, fl.release_date, " + "fl.duration, fl.rating_id, r.name_rating, " + "COUNT(DISTINCT lf.user_id) AS likes_count " + "FROM films AS fl " + "LEFT JOIN rating AS r ON fl.rating_id = r.rating_id " + "LEFT JOIN liked_film AS lf ON fl.film_id = lf.film_id " + "LEFT JOIN film_directors AS fd ON fl.film_id = fd.film_id " + "LEFT JOIN directors AS d ON fd.director_id = d.director_id " + "WHERE LOWER(fl.name) LIKE LOWER(?) " + "OR LOWER(d.name) LIKE LOWER(?) " + "GROUP BY fl.film_id, fl.name, fl.description, fl.release_date, " + "fl.duration, fl.rating_id, r.name_rating " + "ORDER BY likes_count DESC, fl.film_id ASC";
 
     @Override
     public List<Film> getCommonFilms(long userId, long friendId) {
@@ -63,14 +56,6 @@ public class FilmDbStorage extends BaseDbStorage<Film> implements FilmStorage {
         log.info("Найдено {} общих фильмов у пользователей с id={} и id={}", films.size(), userId, friendId);
         return films;
     }
-    private static final String ADD_DIRECTOR_QUERY = "INSERT INTO film_directors (film_id, director_id) VALUES (?, ?)";
-    private static final String GET_FILM_DIRECTORS_QUERY = "SELECT d.director_id, d.name " + "FROM film_directors fd " + "JOIN directors d ON fd.director_id = d.director_id " + "WHERE fd.film_id = ?";
-    private static final String DELETE_FILM_DIRECTORS_QUERY = "DELETE FROM film_directors WHERE film_id = ?";
-    private static final String GET_FILMS_BY_DIRECTOR_YEAR_QUERY = "SELECT fl.*, rt.name_rating " + "FROM films fl " + "LEFT JOIN rating rt ON fl.rating_id = rt.rating_id " + "JOIN film_directors fd ON fl.film_id = fd.film_id " + "WHERE fd.director_id = ? " + "ORDER BY fl.release_date ASC, fl.film_id ASC";
-    private static final String GET_FILMS_BY_DIRECTOR_LIKES_QUERY = "SELECT fl.*, rt.name_rating, COUNT(lf.user_id) AS likes_count " + "FROM films fl " + "LEFT JOIN rating rt ON fl.rating_id = rt.rating_id " + "JOIN film_directors fd ON fl.film_id = fd.film_id " + "LEFT JOIN liked_film lf ON fl.film_id = lf.film_id " + "WHERE fd.director_id = ? " + "GROUP BY fl.film_id, fl.name, fl.description, fl.release_date, " + "fl.duration, fl.rating_id, rt.name_rating " + "ORDER BY likes_count DESC, fl.film_id ASC";
-    private static final String SEARCH_BY_TITLE_QUERY = "SELECT fl.film_id, fl.name, fl.description, fl.release_date, " + "fl.duration, fl.rating_id, r.name_rating, " + "COUNT(DISTINCT lf.user_id) AS likes_count " + "FROM films AS fl " + "LEFT JOIN rating AS r ON fl.rating_id = r.rating_id " + "LEFT JOIN liked_film AS lf ON fl.film_id = lf.film_id " + "WHERE LOWER(fl.name) LIKE LOWER(?) " + "GROUP BY fl.film_id, fl.name, fl.description, fl.release_date, " + "fl.duration, fl.rating_id, r.name_rating " + "ORDER BY likes_count DESC, fl.film_id ASC";
-    private static final String SEARCH_BY_DIRECTOR_QUERY = "SELECT fl.film_id, fl.name, fl.description, fl.release_date, " + "fl.duration, fl.rating_id, r.name_rating, " + "COUNT(DISTINCT lf.user_id) AS likes_count " + "FROM films AS fl " + "LEFT JOIN rating AS r ON fl.rating_id = r.rating_id " + "LEFT JOIN liked_film AS lf ON fl.film_id = lf.film_id " + "JOIN film_directors AS fd ON fl.film_id = fd.film_id " + "JOIN directors AS d ON fd.director_id = d.director_id " + "WHERE LOWER(d.name) LIKE LOWER(?) " + "GROUP BY fl.film_id, fl.name, fl.description, fl.release_date, " + "fl.duration, fl.rating_id, r.name_rating " + "ORDER BY likes_count DESC, fl.film_id ASC";
-    private static final String SEARCH_BY_TITLE_AND_DIRECTOR_QUERY = "SELECT fl.film_id, fl.name, fl.description, fl.release_date, " + "fl.duration, fl.rating_id, r.name_rating, " + "COUNT(DISTINCT lf.user_id) AS likes_count " + "FROM films AS fl " + "LEFT JOIN rating AS r ON fl.rating_id = r.rating_id " + "LEFT JOIN liked_film AS lf ON fl.film_id = lf.film_id " + "LEFT JOIN film_directors AS fd ON fl.film_id = fd.film_id " + "LEFT JOIN directors AS d ON fd.director_id = d.director_id " + "WHERE LOWER(fl.name) LIKE LOWER(?) " + "OR LOWER(d.name) LIKE LOWER(?) " + "GROUP BY fl.film_id, fl.name, fl.description, fl.release_date, " + "fl.duration, fl.rating_id, r.name_rating " + "ORDER BY likes_count DESC, fl.film_id ASC";
 
     public FilmDbStorage(JdbcTemplate jdbc, RowMapper<Film> rowMapper) {
         super(jdbc, rowMapper);
