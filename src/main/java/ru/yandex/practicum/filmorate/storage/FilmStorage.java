fdf0112701cf6a32028159157604d628d33c5dc7
beloysk98 <151573439+beloysk98@users.noreply.github.com>
2026-09-13 21:01:13 +0300

    Update FilmStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -25,7 +25,8 @@ public interface FilmStorage {
 
     boolean checkLike(long filmId, long userId);
 
-    default List<Film> getTopFilms(long count) {return getTopFilms(count, null, null);
+    default List<Film> getTopFilms(long count) {
+        return getTopFilms(count, null, null);
     }
 
     void addGenreFilm(long filmId, long genreId);
